677b6e828f72740049f73fff8f3296ebb79a7a99
rainingchicken <154983686+rainingchicken@users.noreply.github.com>
2024-08-22 09:18:22 -0700

    user auth backend

--- backend-recipe/server.js
+++ backend-recipe/server.js
@@ -2,18 +2,25 @@ import express from "express";
 import dotenv from "dotenv";
 dotenv.config();
 import conn from "./db/conn.js";
-
+import cookieParser from "cookie-parser";
+import { notFound, errorHandler } from "./middleware/errorMiddleware.js";
 const port = process.env.PORT || 4000;
 
 conn();
 
 import recipeRoutes from "./routes/recipeRoutes.js";
+import userRoutes from "./routes/userRoutes.js";
 
 const app = express();
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use(cookieParser());
 
+app.use("/api/user", userRoutes);
 app.use("/api/recipes", recipeRoutes);
+
+app.use(notFound);
+app.use(errorHandler);
 
 app.listen(port, () => console.log(`Server started on port ${port}`));
